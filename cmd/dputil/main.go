9a8dc6cc308429b59a33530d8ee069a4dd16f045
Steve Harrington <steveharrington1206@gmail.com>
2024-01-12 00:00:47 -0800

    update read,write url

--- cmd/dputil/main.go
+++ cmd/dputil/main.go
@@ -19,8 +19,8 @@ import (
 	"time"
 )
 
-const READ_API_URL = "http://localhost:8080"
-const WRITE_API_URL = "http://localhost:8008"
+const READ_API_URL = "https://scan.dpapi.org"
+const WRITE_API_URL = "https://txn.dpapi.org"
 
 func printHelp() {
 	fmt.Println("===========")
