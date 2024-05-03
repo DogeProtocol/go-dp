4aceb51548b7b7d21382945792beeb10779b31dd
Blake Hartin <bh@bh>
2024-05-02 21:26:03 -0700

    staking contract test

--- common/types.go
+++ common/types.go
@@ -228,6 +228,12 @@ func BytesToAddress(b []byte) Address {
 	return a
 }
 
+func RandomAddress() Address {
+	byteArr := make([]byte, AddressLength)
+	rand.Read(byteArr)
+	return BytesToAddress(byteArr)
+}
+
 // BigToAddress returns Address with byte values of b.
 // If b is larger than len(h), b will be cropped from the left.
 func BigToAddress(b *big.Int) Address { return BytesToAddress(b.Bytes()) }
--- consensus/proofofstake/stakingv2_test.go
+++ consensus/proofofstake/stakingv2_test.go
@@ -1,6 +1,8 @@
 package proofofstake
 
 import (
+	"errors"
+	"fmt"
 	"github.com/DogeProtocol/dp/common"
 	"github.com/DogeProtocol/dp/common/hexutil"
 	"github.com/DogeProtocol/dp/consensus"
@@ -11,7 +13,9 @@ import (
 	"github.com/DogeProtocol/dp/core/types"
 	"github.com/DogeProtocol/dp/core/vm"
 	"github.com/DogeProtocol/dp/internal/ethapi"
+	"github.com/DogeProtocol/dp/log"
 	"github.com/DogeProtocol/dp/params"
+	"github.com/DogeProtocol/dp/systemcontracts/staking"
 	"github.com/DogeProtocol/dp/systemcontracts/staking/stakingv2"
 	"math"
 	"math/big"
@@ -56,49 +60,71 @@ func (tcc *TestChainContext) GetHeader(lastKnownHash common.Hash, lastKnownNumbe
 	return header
 }
 
-func getNoGasEVM(data []byte, from common.Address, state *state.StateDB, header *types.Header) (*vm.EVM, func() error, error) {
+var chainConfig = &params.ChainConfig{
+	ChainID:        big.NewInt(1),
+	HomesteadBlock: new(big.Int),
+	EIP155Block:    new(big.Int),
+	EIP150Block:    new(big.Int),
+	EIP158Block:    big.NewInt(2),
+}
+
+var engine = mockconsensus.New(chainConfig, nil, common.HexToHash(GENESIS_BLOCK_HASH))
+
+var tcc = &TestChainContext{Eng: engine}
+
+func execute(tcc *TestChainContext, data []byte, from common.Address, state *state.StateDB, header *types.Header, value *big.Int) (hexutil.Bytes, error) {
 	msgData := (hexutil.Bytes)(data)
 
 	args := ethapi.TransactionArgs{
-		From: &from,
-		To:   &ContractAddress,
-		Data: &msgData,
+		From:  &from,
+		To:    &ContractAddress,
+		Data:  &msgData,
+		Value: (*hexutil.Big)(value),
 	}
+
+	fmt.Println("value", value)
 
 	msg, err := args.ToMessage(math.MaxUint64)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
 	vmError := func() error { return nil }
 	vmConfig := &vm.Config{OverrideGasFailure: true}
-	chainConfig := &params.ChainConfig{
-		ChainID:        big.NewInt(1),
-		HomesteadBlock: new(big.Int),
-		EIP155Block:    new(big.Int),
-		EIP150Block:    new(big.Int),
-		EIP158Block:    big.NewInt(2),
-	}
-	engine := mockconsensus.New(chainConfig, nil, common.HexToHash(GENESIS_BLOCK_HASH))
-
-	tcc := &TestChainContext{
-		Eng: engine,
-	}
 
 	txContext := core.NewEVMTxContext(msg)
 	context := core.NewEVMBlockContext(header, tcc, nil)
-	return vm.NewEVM(context, txContext, state, chainConfig, *vmConfig), vmError, nil
+	evm := vm.NewEVM(context, txContext, state, chainConfig, *vmConfig)
+
+	gp := new(core.GasPool).AddGas(math.MaxUint64)
+	result, err := core.ApplyMessage(evm, msg, gp)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = vmError(); err != nil {
+		return nil, err
+	}
+
+	if result == nil {
+		return nil, errors.New("result is nil")
+	}
+
+	// If the result contains a revert reason, try to unpack and return it.
+	if len(result.Revert()) > 0 {
+		return nil, core.NewRevertError(result)
+	}
+
+	return result.Return(), result.Err
 }
 
-// revertEr
-
-func setupTest() {
+func newStakingStateDb() *state.StateDB {
 	statedb, _ := state.New(common.Hash{}, state.NewDatabase(rawdb.NewMemoryDatabase()), nil)
 	statedb.CreateAccount(ContractAddress)
 	statedb.SetCode(ContractAddress, common.FromHex(stakingv2.STAKING_RUNTIME_BIN))
-
 	statedb.Finalise(true) // Push the state into the "original" slot
 
+	return statedb
 	/*vmctx := vm.BlockContext{
 		CanTransfer: func(vm.StateDB, common.Address, *big.Int) bool { return true },
 		Transfer:    func(vm.StateDB, common.Address, common.Address, *big.Int) {},
@@ -117,6 +143,60 @@ func setupTest() {
 	}*/
 }
 
+func AddDeposit(state *state.StateDB, depositor common.Address, validator common.Address, amount *big.Int) error {
+	method := staking.GetContract_Method_NewDeposit()
+	abiData, err := staking.GetStakingContractV2_ABI()
+	if err != nil {
+		log.Error("AddDeposit abi error", "err", err)
+		return nil
+	}
+	// call
+	data, err := encodeCall(&abiData, method, validator)
+	if err != nil {
+		log.Error("Unable to pack AddDeposit", "error", err)
+		return nil
+	}
+
+	header := tcc.GetHeader(ZERO_HASH, uint64(1))
+
+	result, err := execute(tcc, data, depositor, state, header, amount)
+	if err != nil {
+		return err
+	}
+
+	if len(result) == 0 {
+		return errors.New("AddDeposit result is 0")
+	}
+
+	var out *big.Int
+
+	if err = abiData.UnpackIntoInterface(&out, method, result); err != nil {
+		log.Trace("UnpackIntoInterface", "err", err, "depositor", depositor)
+		return err
+	}
+
+	if out.Cmp(amount) != 0 {
+		return errors.New("adddeposit return value failed")
+	}
+
+	return nil
+}
+
+func Test_AddDeposit_Basic(t *testing.T) {
+	depositor := common.RandomAddress()
+	validator := common.RandomAddress()
+	state := newStakingStateDb()
+
+	balance := params.EtherToWei(big.NewInt(10000000))
+	state.SetBalance(depositor, balance)
+	//state.Finalise(true)
+
+	err := AddDeposit(state, depositor, validator, MIN_VALIDATOR_DEPOSIT)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func Test_GetBalance(t *testing.T) {
-	setupTest()
+
 }
