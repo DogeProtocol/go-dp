1d651b50fb252f4baba1b06846cd8cd49361a3a2
Blake Hartin <bh@bh>
2024-05-14 17:43:42 -0700

    ignore out of order

--- consensus/proofofstake/PacketHandler.go
+++ consensus/proofofstake/PacketHandler.go
@@ -1202,6 +1202,8 @@ func (cph *ConsensusHandler) handleAckBlockProposalPacket(validator common.Addre
 	} else if blockRoundDetails.state == BLOCK_STATE_WAITING_FOR_PROPOSAL {
 	} else if blockRoundDetails.state == BLOCK_STATE_WAITING_FOR_COMMITS {
 
+	} else if blockRoundDetails.state == BLOCK_STATE_RECEIVED_COMMITS {
+
 	} else {
 		log.Debug("handleAckBlockProposalPacket blockRoundDetails", "unexpected state", blockRoundDetails.state)
 		return errors.New("invalid state")
