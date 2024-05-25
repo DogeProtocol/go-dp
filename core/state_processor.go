6a73c68d3acaede2ce3d09872bc851afcf6c19b9
Blake Hartin <bh@bh>
2024-05-24 23:25:49 -0700

    423616

--- core/state_processor.go
+++ core/state_processor.go
@@ -111,7 +111,6 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 	}
 	// Finalize the block, applying any consensus engine specific extras (e.g. block rewards)
 	p.engine.Finalize(p.bc, header, statedb, block.Transactions())
-	panic("done")
 
 	backupManager := backupmanager.GetInstance()
 	if backupManager != nil {
