a480e1e624ad1fdf9866230fdcadbf8ee9c374a1
Blake Hartin <bh@bh>
2024-05-24 22:18:48 -0700

    post state root

--- core/state_processor.go
+++ core/state_processor.go
@@ -103,10 +103,10 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		receipts = append(receipts, receipt)
 		allLogs = append(allLogs, receipt.Logs...)
 
-		log.Info("Tx", "hash", tx.Hash(), "tx data", common.Bytes2Hex(tx.Data()), "usedGas", &usedGas, "from", msg.From().Hex(), "nonce", tx.Nonce(), "GasUsed", receipt.GasUsed,
+		log.Info("Tx", "hash", tx.Hash(), "receipt poststate", common.Bytes2Hex(receipt.PostState), "tx data", common.Bytes2Hex(tx.Data()), "usedGas", &usedGas, "from", msg.From().Hex(), "nonce", tx.Nonce(), "GasUsed", receipt.GasUsed,
 			"CumulativeGasUsed", receipt.CumulativeGasUsed, "Type", receipt.Type,
 			"status", receipt.Status, "Bloom", receipt.Bloom.Bytes(), "ContractAddress", receipt.ContractAddress, "TxHash", receipt.TxHash.Bytes(),
-			"to", tx.To(), "tx", tx.Value().String(), "msg", msg.AccessList(), "receipt", receipt.Logs, "timestamp", block.Time(), "Difficulty", block.Difficulty(), "NumberU64", block.NumberU64())
+			"to", tx.To(), "value", tx.Value().String(), "msg", msg.AccessList(), "receipt", receipt.Logs, "timestamp", block.Time(), "Difficulty", block.Difficulty(), "NumberU64", block.NumberU64())
 
 	}
 	// Finalize the block, applying any consensus engine specific extras (e.g. block rewards)
