97f22569f4500bfb98893a0aba9c273d997f2bf6
Doge Protocol Platform <83708066+DogeProtocol@users.noreply.github.com>
2024-06-06 23:11:22 -0700

    Update main.go v2.0.41

--- cmd/dp/main.go
+++ cmd/dp/main.go
@@ -48,7 +48,7 @@ const (
 	clientIdentifier = "geth" // Client identifier to advertise over the network
 )
 
-const VERSION_NUMBER = "v2.0.40"
+const VERSION_NUMBER = "v2.0.42"
 
 var (
 	// Git SHA1 commit hash of the release (set via linker flags)
